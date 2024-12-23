5d0e3bdc2cdba2165a7e078a4ffec7782ba3b0f1
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2024-12-23 03:33:15 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,8 +2,20 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Work.settings')
+def main():
+    """Run administrative tasks."""
+    # Determine the environment and set the corresponding settings module
+    env = os.getenv('DJANGO_ENV', 'production').lower()
+    
+    if env == 'production':
+        settings_module = 'Work.settings.production'
+    elif env == 'staging':
+        settings_module = 'Work.settings.staging'
+    else:
+        settings_module = 'Work.settings.development'
+    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -12,4 +24,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
