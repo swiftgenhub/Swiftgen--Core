f0fcad9c257ba56b4b2750a3e08a0627e7a38cd6
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2024-12-28 00:59:25 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 def main():
+    # Set the default settings module for the Django application
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-    
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
