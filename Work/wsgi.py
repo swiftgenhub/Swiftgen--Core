6b977beeb6130d7ab6633439fbb9dc371da93f2c
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2025-01-14 02:43:28 +0300

    Update wsgi.py

--- Work/wsgi.py
+++ Work/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Work.settings')
 
 application = get_wsgi_application()
-
-port = os.environ.get('PORT', 8000)  # Ensure your app uses the correct port
