df0988a418713f6a2e07876ac6176030d330fa27
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2025-01-14 02:30:33 +0300

    Update asgi.py

--- Work/asgi.py
+++ Work/asgi.py
@@ -4,10 +4,8 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import wsPattern
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Work.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-http_response_app = get_asgi_application()
+application = get_asgi_application()
 
-application = ProtocolTypeRouter(
-    {"http": http_response_app, "websocket": URLRouter(wsPattern)}
-)
+port = os.environ.get('PORT', 8000)  # Ensure your app uses the correct port
