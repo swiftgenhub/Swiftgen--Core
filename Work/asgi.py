18dd057a0c1b64a19d9e3b82d3b11bd4961deb9e
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2025-01-14 02:33:59 +0300

    Update asgi.py

--- Work/asgi.py
+++ Work/asgi.py
@@ -4,8 +4,9 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import wsPattern
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Work.settings')
 
-application = get_asgi_application()
+application = get_wsgi_application()
 
 port = os.environ.get('PORT', 8000)  # Ensure your app uses the correct port
+  
