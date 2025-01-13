7483d7d007882e5fcfac44b58a16f62bb648f196
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2025-01-13 18:38:22 +0300

    Update settings.py

--- Work/settings.py
+++ Work/settings.py
@@ -95,8 +95,8 @@ WSGI_APPLICATION = 'Work.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',  # SQLite for development
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
