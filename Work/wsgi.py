cc3da17e78165d47edd207f5a1506cfd415044a1
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2025-01-17 02:56:30 +0300

    Update wsgi.py

--- Work/wsgi.py
+++ Work/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Swiftgen_Core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Work.settings')
 
 application = get_wsgi_application()
