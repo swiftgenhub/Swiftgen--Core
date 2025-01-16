dcd4bfb36faf367d6032d8c5b23ebbaff4705d46
SWIFT TALENT FORGE MARKETPLACE <upworkstud198@gmail.com>
2025-01-17 01:53:30 +0300

    Update wsgi.py

--- Work/wsgi.py
+++ Work/wsgi.py
@@ -4,12 +4,12 @@ WSGI config for Work project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 """
 
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Swiftgen--Core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Swiftgen_Core.settings')
 
 application = get_wsgi_application()
